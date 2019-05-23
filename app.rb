5b2c9a81808715678f37a5e24c08bf2282cbe1b4
jtmunoz <jordantmunoz@gmail.com>
2019-05-22 22:39:46 -0700

    adjusts the model from post to note

--- app.rb
+++ app.rb
@@ -3,3 +3,7 @@ require "sinatra/activerecord"
 require "./models.rb"
 
 set :database, "sqlite3:notemakerdb.sqlite3"
+
+get '/' do
+	"Hello World"
+end
--- models.rb
+++ models.rb
@@ -1,2 +1,2 @@
-class Post < ActiveRecord::Base
+class Note < ActiveRecord::Base
 end
