19a1b3c3a94795fa276291d75451db422c2ddbad
jtmunoz <jordantmunoz@gmail.com>
2019-05-30 23:10:49 -0700

    adds index.rb

---
+++ app/controllers/index.rb
@@ -0,0 +1,28 @@
+get '/' do
+	@notes = Note.all
+	erb :index
+end
+
+get "/note/:id" do
+	@note = Note.find(params[:id])
+	erb :note_page
+end
+
+post '/note' do
+	@note = Note.create(title: params[:title], body: params[:body])
+	redirect '/'
+end
+
+put '/note/:id' do
+	@note = Note.find(params[:id])
+	@note.update(title: params[:title], body: params[:body])
+	@note.save
+	redirect '/note/'+params[:id]
+end 
+
+delete '/note/:id' do
+	puts "DELETING"
+	@note = Note.find(params[:id])
+	@note.destroy
+	redirect '/'
+end
