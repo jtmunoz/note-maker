463ff636230f42bbf465af2b63a4c9290359433c
jtmunoz <jordantmunoz@gmail.com>
2019-06-12 22:00:32 -0700

    adds note controller, modifies view for new note

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -3,30 +3,3 @@ get '/' do
 	erb :index
 end
 
-get "/notes/new" do
-	erb :"notes/new"
-end
-
-get "/notes/:id" do
-	@note = Note.find(params[:id])
-	erb :note_page
-end
-
-
-post '/notes' do
-	@note = Note.create(title: params[:note][:title], body: params[:note][:body])
-	redirect '/'
-end
-
-put '/notes/:id' do
-	@note = Note.find(params[:id])
-	@note.update(title: params[:note][:title], body: params[:note][:body])
-	@note.save
-	redirect '/notes/'+params[:id]
-end 
-
-delete '/notes/:id' do
-	@note = Note.find(params[:id])
-	@note.destroy
-	redirect '/'
-end
